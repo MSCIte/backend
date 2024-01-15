602c596ad18ef4dd57c4fcff92d5c1686a1da025
Christian Chan <christian.chan325@gmail.com>
2024-01-14 19:32:27 -0500

    inital db migration setup alembic

--- database/models.py
+++ database/models.py
@@ -18,8 +18,8 @@ course_antirequisites = Table('course_antirequisites', Base.metadata,
 )
 
 
-
 class Course(Base):
+    __allow_unmapped__ = True
     __tablename__ = 'courses'
     id = Column(Integer, primary_key=True)
     course_code: Column(String, unique=True, index=True)
@@ -32,10 +32,14 @@ class Course(Base):
 
 
 class Options(Base):
+    __allow_unmapped__ = True
     __tablename__ = 'options'
+    id = Column(Integer, primary_key=True)
     option_name: Column(String, index=True)
 
 
 class EngineeringDiscipline(Base):
+    __allow_unmapped__ = True
     __tablename__ = 'engineering_discipline'
+    id = Column(Integer, primary_key=True)
     discipline_name: Column(String, unique=True, index=True)
--- database/schema.py
+++ database/schema.py
@@ -1,22 +1,24 @@
 from typing import Optional, Annotated
 from fastapi_camelcase import CamelModel
 from fastapi import Depends
+from .database import Base
+from sqlalchemy.orm import Mapped
 
 
-class Course(CamelModel):
-    course_code: str
-    course_name: str
-    credit: int
-    description: str
-    location: str
+class Course(Base):
+    course_code: Mapped[str]
+    course_name: Mapped[str]
+    credit: Mapped[int]
+    description: Mapped[str]
+    location: Mapped[str]
 
 
-class Options(CamelModel):
-    option_name: str
+class Options(Base):
+    option_name: Mapped[str]
 
 
-class EngineeringDiscipline(CamelModel):
-    discipline_name: str
+class EngineeringDiscipline(Base):
+    discipline_name: Mapped[str]
 
 
 
