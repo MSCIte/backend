c7f40cf23f9c4cba3b128ea57d515d12f13ff513
Kevin Jiang <kevin51jiang@email.com>
2024-03-06 10:23:51 -0500

    skull

--- db/database.py
+++ db/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from src.config import get_env
 
 engine = create_engine(
-    get_env()["SQLALCHEMY_DATABASE_URL"]
+    get_env()["SQLALCHEMY_DATABASE_URL"],
+    pool_size=40,
+    max_overflow=50
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
