8e726b4aaf27fd855e4757b1fa3a88e76be5186a
Kevin Jiang <kevin51jiang@email.com>
2024-03-06 10:29:09 -0500

    rollback the enhanced pooling

--- db/database.py
+++ db/database.py
@@ -5,9 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from src.config import get_env
 
 engine = create_engine(
-    get_env()["SQLALCHEMY_DATABASE_URL"],
-    pool_size=40,
-    max_overflow=50
+    get_env()["SQLALCHEMY_DATABASE_URL"]
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
